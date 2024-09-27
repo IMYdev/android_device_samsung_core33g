e82ae66dc9ab7ab9c3065b1363b493786e8518f0
IMYdev <121319594+IMYdev@users.noreply.github.com>
2024-09-27 17:20:25 +0200

    cm -> lineage

--- lineage.mk
+++ lineage.mk
@@ -1,18 +1,18 @@
 ## Specify phone tech before including full_phone	
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/lineage/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := core33g
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/device_core33g.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := core33g
-PRODUCT_NAME := cm_core33g
+PRODUCT_NAME := lineage_core33g
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G360H
 PRODUCT_MANUFACTURER := samsung
